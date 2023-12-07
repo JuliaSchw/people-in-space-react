01d8c31b681bee1ef684ac358a687b0ae63c478a
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-07 16:16:17 +0100

    add use effect bc the api was fetched 1000 times and it could lead to an error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [peopleInSpace, setPeopleInSpace] = useState(0);
+  const [namesInSpace, setNamesInSpace] = useState([]);
 
   const fetchData = async () => {
     const response = await fetch("http://api.open-notify.org/astros.json");
     const data = await response.json();
+
     setPeopleInSpace(data.number);
+
+    const names = data.people.map((person) => person.name);
+    setNamesInSpace(names);
   };
 
-  fetchData();
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1> 🚀 People in Space = {peopleInSpace} 🚀 </h1>
+        <ul>
+          {namesInSpace.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
